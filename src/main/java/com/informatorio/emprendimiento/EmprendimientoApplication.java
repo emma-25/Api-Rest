fc31a7b26633a22883821c413b5cccd925617dd5
Your Name <mombellitic@gmail.com>
2021-12-20 14:54:15 -0300

    agregando archivo base

---
+++ src/main/java/com/informatorio/emprendimiento/EmprendimientoApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.emprendimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprendimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprendimientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/emprendimiento/EmprendimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.emprendimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprendimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
